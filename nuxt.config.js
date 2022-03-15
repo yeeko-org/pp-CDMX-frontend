a33899c4404b8d0e6ba177a4cdeb8041cdcfacf3
rickrebel <rickrebel@gmail.com>
2022-03-15 09:42:29 -0600

    Cambio título de página

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,11 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: 'Presupuesto Participativo CDMX',
+    title: 'Plataforma proyectos de cambio anticorrupción',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Conoce más sobre el Presupuesto Participativo de la CDMX' }
+      { hid: 'description', name: 'description', content: 'Plataforma para SESEAs' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
